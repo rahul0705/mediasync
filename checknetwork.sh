1a21e6cda530cf9aee984f828c33d90256ef318c
rahul0705 <rahul0705@gmail.com>
2013-02-24 16:34:16 -0500

    Got rid of checknetwork

--- checknetwork.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-x=`ping -c1 google.com 2>&1 | grep unknown`
-echo `date` >> /home/pi/logs/network.log
-if [ ! "$x" = "" ]; then
-	echo "Network down"  >> /home/pi/logs/network.log
-else
-	echo "Network up"  >> /home/pi/logs/network.log
-fi
