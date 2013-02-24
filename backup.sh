3b2afc3d932189f5a7e69a13d9a2d7f7970d2482
rahul0705 <rahul0705@gmail.com>
2013-02-24 16:32:53 -0500

    Joined checknetwork with backup

--- backup.sh
+++ backup.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
+
 FLOCK=/usr/bin/flock
-RLFILE=/home/pi/scripts/.read_lock_file
-WLFILE=/home/pi/scripts/.write_lock_file
 RSYNC=/usr/bin/rsync
 SSH=/usr/bin/ssh
+PING=/usr/bin/ping
+DATE=/usr/bin/date
+RLFILE=/home/pi/scripts/.read_lock_file
+WLFILE=/home/pi/scripts/.write_lock_file
+SERVER="google.com"
+CACHE="$HOME/recent"
+NETLOG="$HOME/logs/network.log"
+PULLLOG="$HOME/logs/pull.log"
+PUSHLOG="$HOME/logs/push.log"
 
-$FLOCK -n $RLFILE -c "$RSYNC -ravz --log-file=/home/pi/logs/backup.log --remove-source-files --exclude=".*" -e "$SSH" buu:/home/rahul/videos/Scene/ /home/pi/backup/"
-$FLOCK -n $WLFILE -c "rm -r /home/pi/recent/"
-$FLOCK -n $WLFILE -c "mkdir /home/pi/recent/"
+netcheck=`$PING -c1 $SERVER 2>&1 | grep unknown`
+
+echo `$DATE` >> /home/pi/logs/network.log
+if [ ! "$netcheck" = "" ]; then
+	echo "Network down"  >> "$NETLOG"
+else
+	echo "Network up"  >> "$NETLOG"
+fi
+
+$FLOCK -n $RLFILE -c "$RSYNC -ravz --log-file="$PULLLOG" --remove-source-files --exclude=".*" buu:/home/rahul/videos/Scene/ /home/pi/backup/"
+$FLOCK -n $WLFILE -c "rm -r $CACHE"
+$FLOCK -n $WLFILE -c "mkdir $CACHE"
+
 files=`ls -t $(find /home/pi/backup/ -type f) | head -n 50`
+
 for original_file in $files 
 do
 	file=`echo $original_file | sed -e 's/\/home\/pi\/backup\///'`
@@ -16,10 +35,10 @@ do
 	filename="$(basename "$file")"
 	if [ ! -d "/home/pi/recent/$directory" ]
 	then
-		$FLOCK -n $WLFILE -c "mkdir -p /home/pi/recent/$directory"
+		$FLOCK -n $WLFILE -c "mkdir -p $CACHE/$directory"
 	fi
-	$FLOCK -n $WLFILE -c "ln -s $original_file /home/pi/recent/$directory/$filename"
+	$FLOCK -n $WLFILE -c "ln -s $original_file $CACHE/$directory/$filename"
 done
 
-$FLOCK -n $WLFILE -c "$RSYNC -PravzL --delete --log-file=/home/pi/logs/push.log /home/pi/recent/ duo:/home/pi/backup/"
-$FLOCK -n $WLFILE -c "rm -r /home/pi/recent/"
+$FLOCK -n $WLFILE -c "$RSYNC -PravzL --delete --log-file=$PUSHLOG $CACHE duo:/home/pi/backup/"
+$FLOCK -n $WLFILE -c "rm -r $CACHE"
