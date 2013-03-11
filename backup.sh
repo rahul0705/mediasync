b74ce1d129d28eda8cde6470300abe11d6f5b34f
rahul0705 <rahul0705@gmail.com>
2013-03-11 15:21:06 -0400

    added usage

--- backup.sh
+++ backup.sh
@@ -16,6 +16,19 @@ NETLOG="$HOME/logs/network.log"
 PULLLOG="$HOME/logs/pull.log"
 PUSHLOG="$HOME/logs/push.log"
 
+usage(){
+    echo "Usage: ./script.sh source destination server"
+    echo
+    echo "source: must be full path"
+    echo -e "\texample: /home/<user>/backupdir/"
+    echo
+    echo "destination: must be full path"
+    echo -e "\texample: /home/<user>/backup/"
+    echo
+    echo "server must be a full domain name"
+    echo -e "\texample: test.example.com"
+}
+
 clean(){
     rm "$HOME/logs/*"
 }
@@ -33,25 +46,26 @@ isServerUp(){
     fi
 }
 
-$FLOCK -n $RLFILE -c "$RSYNC -ravz --log-file="$PULLLOG" --remove-source-files --exclude=".*" buu:/home/rahul/videos/Scene/ /home/pi/backup/"
-$FLOCK -n $WLFILE -c "rm -r $CACHE"
-$FLOCK -n $WLFILE -c "mkdir $CACHE"
+backup(){
+    $FLOCK -n $RLFILE -c "$RSYNC -ravz --log-file="$PULLLOG" --remove-source-files --exclude=".*" buu:/home/rahul/videos/Scene/ /home/pi/backup/"
+    $FLOCK -n $WLFILE -c "rm -r $CACHE"
+    $FLOCK -n $WLFILE -c "mkdir $CACHE"
 
-files=`ls -t $(find /home/pi/backup/ -type f) | head -n 50`
+    files=`ls -t $(find /home/pi/backup/ -type f) | head -n 50`
 
-for original_file in $files 
-do
-    file=`echo $original_file | sed -e 's/\/home\/pi\/backup\///'`
-    directory="$(dirname "$file")"
-    filename="$(basename "$file")"
-    if [ ! -d "/home/pi/recent/$directory" ]
-    then
-        $FLOCK -n $WLFILE -c "mkdir -p $CACHE/$directory"
-    fi
-    $FLOCK -n $WLFILE -c "ln -s $original_file $CACHE/$directory/$filename"
-done
+    for original_file in $files 
+    do
+        file=`echo $original_file | sed -e 's/\/home\/pi\/backup\///'`
+        directory="$(dirname "$file")"
+        filename="$(basename "$file")"
+        if [ ! -d "/home/pi/recent/$directory" ]
+        then
+            $FLOCK -n $WLFILE -c "mkdir -p $CACHE/$directory"
+        fi
+        $FLOCK -n $WLFILE -c "ln -s $original_file $CACHE/$directory/$filename"
+    done
 
-$FLOCK -n $WLFILE -c "$RSYNC -PravzL --delete --log-file=$PUSHLOG $CACHE duo:/home/pi/backup/"
-$FLOCK -n $WLFILE -c "rm -r $CACHE"
-#!/bin/bash
-
+    $FLOCK -n $WLFILE -c "$RSYNC -PravzL --delete --log-file=$PUSHLOG $CACHE duo:/home/pi/backup/"
+    $FLOCK -n $WLFILE -c "rm -r $CACHE"
+}
+usage;
