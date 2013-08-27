4d15a77d19c0c1a464f1b4a29ff5ae1e2b1b7031
Rahul Mohandas <rahul@buu.rahulmohandas.com>
2013-08-27 18:44:26 +0200

    Changed regex for renaming so that it can be more robust. Rsync now works with inotify more testing needs to be done.

--- backup.sh
+++ backup.sh
@@ -5,19 +5,15 @@ RSYNC=/usr/bin/rsync
 SSH=/usr/bin/ssh
 PING=/usr/bin/ping
 DATE=/usr/bin/date
-SRC=$1
-DEST=$2
-RLFILE="$HOME/scripts/.read_lock_file"
+RLFILE="$HOME/scripts/.rsync_lock_file"
 WLFILE="$HOME/scripts/.write_lock_file"
 
 CACHE="$HOME/recent"
 
-NETLOG="$HOME/logs/network.log"
-PUSHLOG="$HOME/logs/push.log"
-NAMELOG="$HOME/logs/rename.log"
+LOG="/var/log/mediaSync.log"
 
 usage(){
-    echo "Usage: ./script.sh source server:destintion"
+    echo "Usage: ./script.sh [-hr] source [server:]destintion"
     echo
     echo "source: must be full path"
     echo -e "\texample: /home/<user>/backupdir/"
@@ -27,27 +23,21 @@ usage(){
     echo
     echo "destination: must be full path"
     echo -e "\texample: /home/<user>/backup/"
+    echo
+    echo "flags:"
+    echo -e "\t-h, --help"
+    echo -e "\t\tthis menu"
+    echo -e "\t-r, --rename"
+    echo -e "\t\trename all files in source to tv and movie folders located at destination"
+    exit 0
 }
 
 clean(){
-    rm "$HOME/logs/*"
+    rmdir $SRC/*
+    echo "cleaned"
 }
 
-isServerUp(){
-    server=`echo $DEST | cut -d: -f1`
-    netcheck=`$PING -c1 $server 2>&1 | grep unknown`
-
-    echo `$DATE` >> "$NETLOG"
-    if [ ! "$netcheck" = "" ]; then
-        echo "Network down"  >> "$NETLOG"
-        return 0;
-    else
-        echo "Network up"  >> "$NETLOG"
-        return 1;
-    fi
-}
-
-backup(){
+sendMedia(){
     $FLOCK -n $RLFILE -c "$RSYNC --recursive --partial --perms --times --group --owner --verbose --compress --log-file="$PUSHLOG" --remove-source-files --exclude=".*" $SRC $DEST"
 }
 
@@ -55,7 +45,7 @@ recentCache(){
     $FLOCK -n $WLFILE -c "rm -r $CACHE"
     $FLOCK -n $WLFILE -c "mkdir $CACHE"
 
-    files=`ls -t $(find /home/pi/backup/ -type f) | head -n 50`
+    files=`ls -t $(find $SRC -type f) | head -n 50`
 
     for original_file in $files 
     do
@@ -69,26 +59,25 @@ recentCache(){
         $FLOCK -n $WLFILE -c "ln -s $original_file $CACHE/$directory/$filename"
     done
 
-    $FLOCK -n $WLFILE -c "$RSYNC -PravzL --delete --log-file=$PUSHLOG $CACHE duo:/home/pi/backup/"
+    $FLOCK -n $WLFILE -c "$RSYNC -PravzL --delete --log-file=$LOG $CACHE $DEST"
     $FLOCK -n $WLFILE -c "rm -r $CACHE"
 }
 
-change(){
-    echo `date` >> $NAMELOG
-    
+renameMedia(){
+    echo "hello"
     #lowercase
-    echo `rename -v y/A-Z/a-z/ $SRC/*` >> $NAMELOG
+    logger -t mediaSync "`rename -v y/A-Z/a-z/ $SRC/*`"
 
+    echo "hello"
     #get rid of Scene group
-    echo `rename -v s/\-.*\.\(\[a-z\]\{3\}\)/\.\\$1/ $SRC/*` >> $NAMELOG
+    logger -s -t mediaSync "`rename -v s/\-.*.\.\(\[a-z\|0-9\]\{3\}$\)/\.\$1/ $SRC/*`"
 
-    #find all files in SRC exclude rename.sh
-    files=`find $SRC -maxdepth 1 -type f \( ! -iname ".*" \) | grep -v rename.sh`
+    #find all files in SRC
+    files=`find $SRC -maxdepth 1 -type f \( ! -iname ".*" \)`
 
     for original_file in $files
     do
         #get rid of SRC in string
-        #file=`echo $original_file | sed -e s/$SRC\///`
         file=`basename $original_file`
 	
 	#check if file has regex for a tv show
@@ -101,11 +90,11 @@ change(){
             if [ ! -d "$SRC/movies" ]
             then
                 mkdir $SRC/movies
-                echo "CREATED $SRC/movies FOLDER" >> $NAMELOG
+                logger -s -t mediaSync "CREATED $SRC/movies FOLDER"
             fi
 
             #move file into $SRC/movies
-            echo `mv -v $original_file $SRC/movies` >> $NAMELOG
+            logger -s -t mediaSync "`mv -v $original_file $SRC/movies`"
 
             #TV SHOW
         else
@@ -116,18 +105,68 @@ change(){
             if [ ! -d "$SRC/tv" ]
             then
                 mkdir $SRC/tv
-                echo "CREATED $SRC/tv FOLDER" >> $NAMELOG
+                logger -s -t mediaSync "CREATED $SRC/tv FOLDER"
             fi
 
             #check if $SRC/tv/$showname exists if not make it
             if [ ! -d "$SRC/tv/$showname" ]
             then
                 mkdir $SRC/tv/$showname
-                echo "CREATED $SRC/tv/$showname FOLDER" >> $NAMELOG
+                logger -s -t mediaSync "CREATED $SRC/tv/$showname FOLDER"
             fi
 
             #move file to $SRC/tv/$showname
-            echo `mv -v $original_file $SRC/tv/$showname` >> $NAMELOG
+            logger -s -t mediaSync "`mv -v $original_file $SRC/tv/$showname`"
         fi
-    done 
+    done
 }
+
+watchMedia(){
+	echo "$SRC"
+	inotifywait -m -e close_write "$SRC" |
+		while read dir event file
+		do
+			echo "$dir $event $file"
+			if [ $renameFlag ]
+			then
+				renameMedia $file
+			fi
+			sendMedia
+			clean
+		done
+}
+
+main(){
+	ARGS=$(getopt -o hr -l "help,rename" -n "mediaSync.sh" -- "$@");
+
+	#Bad arguments
+	if [ $? -ne 0 ]
+	then
+		exit 1
+	fi
+
+	eval set -- "$ARGS";
+
+	while true
+	do
+  		case "$1" in
+    		-h|--help)
+      			shift;
+      			usage;
+      			;;
+    		-r|--rename)
+      			shift;
+      			renameFlag=true;
+      			;;
+    		--)
+      			shift;
+      			break;
+      			;;
+		esac
+	done
+	SRC=$1
+	DEST=$2
+	watchMedia $renameFlag
+}
+
+main "$@"
